4bdc74d1c9121cb9e20307da89f2cd852ebd8230
Christian <seorefchristian@gmail.com>
2025-05-25 14:25:42 +0300

    imageProfile Solved

---
+++ src/Form/StudentForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Student;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StudentForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('imageprofile', FileType::class, [
+                'label'=>'Photo de profil',
+                'mapped'=>false,
+            ])
+            ->add('name', TextType::class,[
+                'label'=>'Nom',
+                
+
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Student::class,
+        ]);
+    }
+}
